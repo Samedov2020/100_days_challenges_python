aa8b47f9ed4e253a9540a4b153fe8f804654049d
Ramil Samadov <90143263+Samedov2020@users.noreply.github.com>
2023-11-27 16:53:23 +0100

    Add files via upload

---
+++ Day-1/two_digit_number.py
@@ -0,0 +1,16 @@
+import math
+
+two_digit_number=int(input("Enter two digit number"))
+first_digit=two_digit_number / 10
+second_digit = two_digit_number % 10
+print(math.floor(first_digit) + second_digit)
+
+
+
+
+# two_digit_number = input()
+# first_digit = int(two_digit_number[0])
+# second_digit = int(two_digit_number[1])
+# # Add the two integers together
+# two_digit_number = first_digit + second_digit
+# print(two_digit_number)
