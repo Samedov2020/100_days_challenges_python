5784acb626b76b67c76d827f92bfd071e0fc8ce1
Ramil Samadov <90143263+Samedov2020@users.noreply.github.com>
2023-11-30 23:57:23 +0100

    Update rock_paper_scissors.py

--- Day-4/rock_paper_scissors.py
+++ Day-4/rock_paper_scissors.py
@@ -28,18 +28,25 @@ scissors = '''
 '''
 game_images=[rock,paper,scissors]
 user_choice=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors"))
-print(game_images[user_choice])
-computer_choice=random.randint(0,2)
-print(f"Computer's choice:{game_images[computer_choice]} \n")
 if user_choice>=3 or user_choice<0:
     print("You typed an invalid number,You lose!")
-elif user_choice==0 and computer_choice==2:
-    print("You win!")
-elif user_choice==2 and computer_choice==2:
-    print("You lose!")
-elif computer_choice>user_choice:
-    print("You lose!")
-elif user_choice>computer_choice:
-    print("You win!")
-elif computer_choice==user_choice:
-    print("It is a draw!")
+else:
+    print(game_images[user_choice])
+    computer_choice = random.randint(0, 2)
+    print(f"Computer's choice:{game_images[computer_choice]} \n")
+
+    if user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif user_choice == 2 and computer_choice == 2:
+        print("You lose!")
+    elif computer_choice > user_choice:
+        print("You lose!")
+    elif user_choice > computer_choice:
+        print("You win!")
+    elif computer_choice == user_choice:
+        print("It is a draw!")
+
+
+
+
+
