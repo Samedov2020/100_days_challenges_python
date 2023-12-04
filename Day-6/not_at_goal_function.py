0980ccfb154dccd8124380974c9442e58a43468e
Ramil Samadov <90143263+Samedov2020@users.noreply.github.com>
2023-12-04 19:04:22 +0100

    Add files via upload

---
+++ Day-6/not_at_goal_function.py
@@ -0,0 +1,16 @@
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+
+while not at_goal():
+    jump()
