32d271405ce7e80a17bab9e9688af4b300261710
Ramil Samadov <90143263+Samedov2020@users.noreply.github.com>
2023-12-12 15:01:32 +0400

    Add files via upload

---
+++ Day-7/guess_the_letter(2.0).py
@@ -0,0 +1,27 @@
+word_list=["umbrella","waterfall","hairdresser"]
+import random
+random_choice=random.choice(word_list)
+guessed_list=[]
+word_length=len(random_choice)
+for _ in range(word_length):
+    guessed_list+="_"
+print(guessed_list)
+end_of_game=False
+while not end_of_game:
+    guess=input("Guess the letter of the word").lower()
+    
+    for position in range(word_length):
+        letter=random_choice[position]
+        if letter==guess:
+            guessed_list[position]=letter
+            
+    print(guessed_list)
+    
+    if "_" not in guessed_list:
+        end_of_game=True
+        print("You win!")
+        
+    
+
+
+
