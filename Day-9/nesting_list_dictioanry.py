958c66f6d37b10f4e2db6f4ec6d06beee03c6d57
Ramil Samadov <90143263+ramilsamadov@users.noreply.github.com>
2024-01-03 15:16:39 +0400

    Add files via upload

---
+++ Day-9/nesting_list_dictioanry.py
@@ -0,0 +1,12 @@
+#Nesting
+capitals={"France":"Paris","Germany":"Berlin","Azerbaijan":"Baku"}
+
+#Nesting a dictionary in dictionary
+travel_log={"France":{"cities_visited":["Paris","Lille","Dijon"]},
+            "Germany":{"cities_visited":["Berlin","Hamburg"]}
+
+#Nesting dictionary in list
+travel_log=[
+    {"France":"Paris","cities_visited":["Paris","Lille","Dijon"]},
+    {"Germany":"Berlin","cities_visited":["Berlin","Hamburg"]}
+    ]
