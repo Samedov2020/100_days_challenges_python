c884145de90527a9376782aeb3194eed3e86670b
Ramil Samadov <90143263+ramilsamadov@users.noreply.github.com>
2024-01-05 03:40:28 +0400

    Add files via upload

---
+++ Day-10/docstrings.py
@@ -0,0 +1,7 @@
+# Comment
+""" 
+Docstrings
+Python docstrings are the string literals that appear right after the definition of a function, method, class,
+or module. They are used to document our code. 
+
+"""
