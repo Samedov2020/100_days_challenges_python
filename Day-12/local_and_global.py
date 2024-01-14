338478cc750b1c2ca220557d82c95f15386d0464
Ramil Samadov <90143263+ramilsamadov@users.noreply.github.com>
2024-01-14 13:09:52 +0100

    Update local_and_global.py

--- Day-12/local_and_global.py
+++ Day-12/local_and_global.py
@@ -1,6 +1,7 @@
 #Scope Global and Local 
 
 #Global scopes are available both inside and outside the function.but Local scopes are available only inside the function
+#There is no block scope
 
 player_health=10  #global scope
 
